80e2e39070a3962b833366442c53f2064c70ef2f
Jingwei Zhang <jwdaniel.zhang@mail.utoronto.ca>
2018-07-06 09:56:13 +0800

    Initial Commit

---
+++ assembly_game_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assembly_game_project
+//
+//  Created by 张经纬 on 2018/7/6.
+//  Copyright © 2018 Jingwei Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
